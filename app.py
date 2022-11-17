8e3feddc0f6f726b9b1fcd4f777cf367e304477f
waltervolpi <117013098+waltervolpi@users.noreply.github.com>
2022-11-17 18:45:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+import datetime
+
+
+class HelloWorld(Resource):
+    def get(self):
+        data_s=str(datetime.datetime.now())
+        return {'hello': data_s}
 
 app = Flask(__name__)
+CORS(app)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Ciao Mondo'
+api.add_resource(HelloWorld, '/')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
